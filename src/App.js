a1e6aa9cb74eacd8b6036130fd7411401b810c1b
elale1984 <elale@mygcu.edu>
2022-06-12 10:21:20 -0500

    some work in home.css file to give specific design colors and values

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
-
+import {Routes, Route} from 'react-router-dom';
+import Contact from "./containers/contact";
+import Resume from "./containers/resume";
+import Portfolio from "./containers/portfolio";
+import Home from "./containers/home";
+import About from "./containers/about";
 function App() {
   return (
     <div className="App">
+      {/* particles js */}
+      {/* navbar */}
+      {/* main page content */}
 
+      <Routes>
+        <Route index path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/resume' element={<Resume/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/portfolio' element={<Portfolio/>} />
+        <Route path='/skills' element={<skills/>} />
+
+
+
+      </Routes>
     </div>
   );
 }
