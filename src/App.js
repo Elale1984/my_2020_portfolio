b6521646050af90e39b119bfaac3c4c8d8527864
elale1984 <elale@mygcu.edu>
2022-06-14 00:20:24 -0500

    update nav bar

--- src/App.js
+++ src/App.js
@@ -5,21 +5,27 @@ import Resume from "./containers/resume";
 import Portfolio from "./containers/portfolio";
 import Home from "./containers/home";
 import About from "./containers/about";
+import NavBar from "./components/navBar";
+import Skills from './containers/skills';
 function App() {
   return (
     <div className="App">
       {/* particles js */}
       {/* navbar */}
+
+      <NavBar/>
+
       {/* main page content */}
 
       <Routes>
         <Route index path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
+        <Route path='/skills' element={<Skills/>} />
         <Route path='/resume' element={<Resume/>} />
         <Route path='/contact' element={<Contact/>} />
         <Route path='/portfolio' element={<Portfolio/>} />
-        <Route path='/skills' element={<skills/>} />
-
+        
+      
 
 
       </Routes>
