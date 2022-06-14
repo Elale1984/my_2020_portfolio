da8479a68ad66dcda1e8aa4b28b4a930110f67f7
elale1984 <elale@mygcu.edu>
2022-06-14 00:59:29 -0500

    restrict the particles to only the home page

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-import './App.css';
-import {Routes, Route} from 'react-router-dom';
+import "./App.css";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Contact from "./containers/contact";
 import Resume from "./containers/resume";
 import Portfolio from "./containers/portfolio";
 import Home from "./containers/home";
 import About from "./containers/about";
 import NavBar from "./components/navBar";
-import Skills from './containers/skills';
-import particles from './utils.js/particles';
+import Skills from "./containers/skills";
+import particles from "./utils.js/particles";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 function App() {
-  const handleInit = async (main)=>
-    await loadFull(main)
+  const location = useLocation();
+  const handleInit = async (main) => await loadFull(main);
+
+  const renderParticleJsInHomePage = location.pathname === "/";
+
   return (
-  
     <div className="App">
       {/* particles js */}
-      <Particles id="particles" options={particles} init={handleInit}/>
+
+      {renderParticleJsInHomePage && (
+        <Particles id="particles" options={particles} init={handleInit} />
+      )}
+
       {/* navbar */}
 
-      <NavBar/>
+      <NavBar />
 
       {/* main page content */}
 
       <Routes>
-        <Route index path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/skills' element={<Skills/>} />
-        <Route path='/resume' element={<Resume/>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/portfolio' element={<Portfolio/>} />
-        
-      
-
-
+        <Route index path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
     </div>
   );
--- src/utils.js/particles.js
+++ src/utils.js/particles.js
@@ -70,4 +70,4 @@ background: {
     },
   },
   detectRetina: true,
-};
+}
