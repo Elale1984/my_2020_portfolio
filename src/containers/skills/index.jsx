e5bc816a19cde182b30b16aeb59091669374b6d5
elale1984 <elale@mygcu.edu>
2022-06-19 00:17:42 -0500

    Skills page and SCSS finished

--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -1,15 +1,61 @@
 import React from "react";
 import PageHeaderContent from "../../components/pageHeader";
-import {BsInfoCircleFill} from 'react-icons/bs'
+import { BsInfoCircleFill } from "react-icons/bs";
+import { skillData } from "./utils";
+import { Animate, AnimateKeyframes } from "react-simple-animate";
+import { Line } from "rc-progress";
+import './styles.scss';
 
-const Skills=()=>{
-    return(
-        <section id="skills" className="skills">
-        <PageHeaderContent
-          headerText="Skills"
-          icon={<BsInfoCircleFill size={40} />}
-        />
-      </section>
-    )
-}
+const Skills = () => {
+  return (
+    <section id="skills" className="skills">
+      <PageHeaderContent
+        headerText="Skills"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="skills__content-wrapper">
+        {skillData.map((item, i) => (
+          <div key={i} className="skills__content-wrapper__inner-content">
+            <Animate
+              play
+              duration={1}
+              delay={0}
+              start={{
+                transform: "translateY(700px)",
+              }}
+              end={{
+                transform: "translateY(0px)",
+              }}
+            >
+              <h3 className="skills__content-wrapper__inner-content__category-text">
+                {item.label}
+              </h3>
+              <div className="skills__content-wrapper__inner-content__progressbar-container">
+                {item.data.map((skillItem, j) => (
+                  <AnimateKeyframes
+                    play
+                    duration={1}
+                    keyframes={["opacity : 1", "opacity : 0"]}
+                    iterationCount="1"
+                  >
+                    <div  key={j} className="progressbar-wrapper">
+                      <p>{skillItem.skillName}</p>
+                      <Line
+                        percent={skillItem.percentage}
+                        strokeWidth="2"
+                        strokeColor="var(--yellow-theme-main-color)"
+                        trailWidth="2"
+                        strokeLinecap="square"
+                      />
+                    </div>
+                  </AnimateKeyframes>
+                ))}
+              </div>
+            </Animate>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 export default Skills;
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,80 @@
+
+
+export const skillData = [
+    {
+        label: "FRONT END",
+        data: [
+            {
+                skillName: "HTML",
+                percentage: "90"
+            },
+            {
+                skillName: "CSS",
+                percentage: "80"
+            },
+            {
+                skillName: "SCSS",
+                percentage: "80"
+            },
+            {
+                skillName: "JAVASCRIPT",
+                percentage: "70"
+            },
+            {
+                skillName: "REACT JS",
+                percentage: "60"
+            },          
+        ],
+    },
+    {
+    label: "BACK END",
+        data: [
+            {
+                skillName: "JAVA",
+                percentage: "95"
+            },
+            {
+                skillName: "KOTLIN",
+                percentage: "70"
+            },
+            {
+                skillName: "C#",
+                percentage: "80"
+            },
+            {
+                skillName: "C++",
+                percentage: "20"
+            },
+            {
+                skillName: "PYTHON",
+                percentage: "80"
+            },
+        ],
+    },
+    {
+        label: "OTHER SKILLS",
+        data: [
+            {
+                skillName: "ANDROID MOBILE APP DEVELOPMENT",
+                percentage: "80"
+            },
+            {
+                skillName: "FIREBASE",
+                percentage: "85"
+            },
+            {
+                skillName: "ROOM DATABASE",
+                percentage: "50"
+            },
+            {
+                skillName: "MYSQL",
+                percentage: "65"
+            },
+            {
+                skillName: "Agile",
+                percentage: "80"
+            },
+           
+        ],
+    },
+]
