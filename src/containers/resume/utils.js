4907c76f48a2fcf9dc77632dd2dbe1840e35a819
elale1984 <elale@mygcu.edu>
2022-06-24 01:24:21 -0500

    completed resume

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,31 @@
+export const data = {
+    experience: [
+        {
+            title: "Web Developer",
+            subTitle: "Pueblo, Colorado",
+            description: " Created and maintained website",
+            skills: "HTML,  Javascript, and Firebase"
+        },
+        {
+            title: "Android App Developer",
+            subTitle: "Newton, Kansas",
+            description: " Creating App for A local Church",
+            skills: "Java, Firebase, Android Studio, SQL"
+        },
+
+    ],
+    education: [
+        {
+            title: "Highschool",
+            subTitle: "Shawnee Mission West: Overland Park, Kansas",
+            description: "Graduated with 3.6 GPA",
+            skills: "Java, C++, Visual Basic"
+        },
+        {
+            title: "College",
+            subTitle: "Grand Canyon University: Phoenix, Arizona ",
+            description: "Junior Year Current GPA 3.5",
+            skills: "Java, C#, Python, C, Agile, Android Studio"
+        },
+    ],
+}
